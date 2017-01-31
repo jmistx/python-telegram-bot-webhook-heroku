d1b15ebdf43c9b9b5c1934f1bf7b1c9bdbfd9338
Oleg Blokhin <blohin.o.d@gmail.com>
2017-01-31 23:53:39 +0300

    Добавление листенера для телеграм-бота

--- manage.py
+++ manage.py
@@ -1,9 +1,28 @@
+import os
+from telegram.ext import Updater
 from flask import Flask
+
 app = Flask(__name__)
+TOKEN = os.environ.get('TOKEN', 'TOKEN')
+# gunicorn default port is 8000
+PORT = int(os.environ.get('PORT', '8000'))
+updater = Updater(TOKEN)
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN,
+                      webhook_url="https://telegrambotexample31337.herokuapp.com/" + TOKEN)
+print(PORT, TOKEN)
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/{0}/".format(TOKEN))
+def hello():
+    return "hop hay lalalay!"
+
 
 if __name__ == "__main__":
     app.run()
